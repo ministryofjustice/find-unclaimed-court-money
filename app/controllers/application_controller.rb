c6d1f48e11c55d96292953ec2972cbdf3ee140a4
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-02-13 11:08:26 +0000

    pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
-
-  def homepage
-    render "/homepage"
-  end
-
-  def accessibility
-    render "/accessibility"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   get "/healthcheck/ready", to: GovukHealthcheck.rack_response
 
-  get "/accessibility", to: "application#accessibility"
+  get "/accessibility", to: "pages#accessibility"
 
-  root to: "application#homepage"
+  root to: "pages#homepage"
 end
