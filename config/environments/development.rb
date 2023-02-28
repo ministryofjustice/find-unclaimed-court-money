b9b649d2ca4e10ec942447750db7d342f7664013
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2023-02-28 10:37:59 +0000

    no longer required

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << /.*\.?dev\.local/
   config.allowlisted_ips = []
 end
