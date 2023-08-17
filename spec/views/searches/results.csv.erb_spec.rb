3142b78729f05788933c2ee31665ce097fb59985
Meena <meena.modhvadia@digital.justice.gov.uk>
2023-08-17 09:51:39 +0100

    correct syntax errors

--- spec/views/searches/results.csv.erb_spec.rb
+++ spec/views/searches/results.csv.erb_spec.rb
@@ -1,16 +1,15 @@
 RSpec.describe "searches/results.csv.erb" do
-    let(:kase) { create(:case) }
+  let(:kase) { create(:case) }
 
-    it "renders a table of results", :aggregate_failures do
-      assign(:cases, [kase])
-      render
+  it "renders a table of results", :aggregate_failures do
+    assign(:cases, [kase])
+    render
 
-      expect(rendered).to have_text(kase.case_name)
-      expect(rendered).to have_text(kase.account_number)
-      expect(rendered).to have_text(kase.year_carried_over)
-      expect(rendered).to have_text(kase.credit_details)
-      expect(rendered).to have_text(kase.date_account_opened)
-      expect(rendered).to have_text(case_url(kase.account_number))
-
-    end
+    expect(rendered).to have_text(kase.case_name)
+    expect(rendered).to have_text(kase.account_number)
+    expect(rendered).to have_text(kase.year_carried_over)
+    expect(rendered).to have_text(kase.credit_details)
+    expect(rendered).to have_text(kase.date_account_opened)
+    expect(rendered).to have_text(case_url(kase.account_number))
   end
+end
