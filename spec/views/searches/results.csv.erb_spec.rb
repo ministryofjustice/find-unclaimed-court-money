b68eb84220869819b5e4d3e915ee7731b17e1bde
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2024-01-24 08:45:09 +0000

    CDPT-1214 FUM: Update Download Results to CSV file (#170)

--- spec/views/searches/results.csv.erb_spec.rb
+++ spec/views/searches/results.csv.erb_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe "searches/results.csv.erb" do
     assign(:cases, [kase])
     render
 
-    expect(rendered).to have_text(kase.case_name)
     expect(rendered).to have_text(kase.account_number)
-    expect(rendered).to have_text(kase.year_carried_over)
+    expect(rendered).to have_text(kase.case_name)
     expect(rendered).to have_text(kase.credit_details)
     expect(rendered).to have_text(kase.date_account_opened)
+    expect(rendered).to have_text(kase.year_carried_over)
+    expect(rendered).to have_text(kase.initial_dormancy)
+    expect(rendered).to have_text(kase.last_claim_date)
     expect(rendered).to have_text(case_url(kase.account_number))
   end
 end
