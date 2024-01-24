9196d902c2144a607730f3d2d9528f4d0c68051e
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2024-01-24 14:22:22 +0000

    Removed null constraint migration and regenerated db schema. (#171)

--- db/migrate/20240123111445_set_not_null_columns.rb
+++
@@ -1,9 +0,0 @@
-class SetNotNullColumns < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :cases, :account_number, false
-    change_column_null :cases, :year_carried_over, false
-    change_column_null :cases, :case_name, false
-    change_column_null :cases, :date_account_opened, false
-    change_column_null :cases, :case_date, false
-  end
-end
