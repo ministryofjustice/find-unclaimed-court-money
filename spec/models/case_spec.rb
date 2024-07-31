03e3de74b352c3e6bf8d330d486dac814b01836e
Andrew Pepler <andrew.pepler@digital.justice.gov.uk>
2024-07-31 16:19:24 +0100

    Fix rubocop issues

--- spec/models/case_spec.rb
+++ spec/models/case_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe Case do
   subject { create(:case) }
 
-  let(:case_2020) { create(:case, case_date: Date.new(2020, 1, 1)) }
-  let(:case_2021) { create(:case, case_date: Date.new(2021, 1, 1)) }
-  let(:case_2022) { create(:case, case_date: Date.new(2022, 1, 1)) }
+  let(:case_2020) { create(:case, case_date: Date.new(2020, 1, 1)) } # rubocop:disable RSpec/IndexedLet
+  let(:case_2021) { create(:case, case_date: Date.new(2021, 1, 1)) } # rubocop:disable RSpec/IndexedLet
+  let(:case_2022) { create(:case, case_date: Date.new(2022, 1, 1)) } # rubocop:disable RSpec/IndexedLet
 
   it { is_expected.to be_valid }
 
@@ -23,7 +23,7 @@ RSpec.describe Case do
     end
 
     it "only returns cases dated on or after the provided date" do
-      expect(described_class.from_date(Date.new(2021, 1, 1))).to match_array [case_2021, case_2022]
+      expect(described_class.from_date(Date.new(2021, 1, 1))).to contain_exactly(case_2021, case_2022)
     end
   end
 
@@ -35,13 +35,13 @@ RSpec.describe Case do
     end
 
     it "only returns cases dated on or before the provided date" do
-      expect(described_class.to_date(Date.new(2021, 1, 1))).to match_array [case_2020, case_2021]
+      expect(described_class.to_date(Date.new(2021, 1, 1))).to contain_exactly(case_2020, case_2021)
     end
   end
 
   describe ".carried_over" do
-    let(:case_carried_over_1985) { create(:case, year_carried_over: "1985") }
-    let(:case_carried_over_1986) { create(:case, year_carried_over: "1986") }
+    let(:case_carried_over_1985) { create(:case, year_carried_over: "1985") } # rubocop:disable RSpec/IndexedLet
+    let(:case_carried_over_1986) { create(:case, year_carried_over: "1986") } # rubocop:disable RSpec/IndexedLet
 
     before do
       case_carried_over_1985
@@ -49,7 +49,7 @@ RSpec.describe Case do
     end
 
     it "finds cases with the keyword in case_name" do
-      expect(described_class.carried_over("1985")).to match_array [case_carried_over_1985]
+      expect(described_class.carried_over("1985")).to contain_exactly(case_carried_over_1985)
     end
   end
 
@@ -59,15 +59,15 @@ RSpec.describe Case do
     let!(:case_goose) { create(:case, account_number: "contains the keyword GoosE that we are looking for") }
 
     it "finds cases with the keyword in case_name" do
-      expect(described_class.for_term("chiCkeN")).to match_array [case_chicken]
+      expect(described_class.for_term("chiCkeN")).to contain_exactly(case_chicken)
     end
 
     it "finds cases with the keyword in credit_details" do
-      expect(described_class.for_term("turKeY")).to match_array [case_turkey]
+      expect(described_class.for_term("turKeY")).to contain_exactly(case_turkey)
     end
 
     it "finds cases with the keyword in account_number" do
-      expect(described_class.for_term("gOOse")).to match_array [case_goose]
+      expect(described_class.for_term("gOOse")).to contain_exactly(case_goose)
     end
   end
 end
