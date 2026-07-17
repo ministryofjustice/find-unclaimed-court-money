f2abd266bd67c259537a690115b35e192ce320ba
Tony Mckernan <anthony.mckernan@justice.gov.uk>
2026-07-17 13:48:48 +0100

    CDPTKAN-1221 Switch on Content Security Policy

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,10 @@ import { initAll } from "govuk-frontend";
 import "./cookie_banner"
 
 initAll();
+
+document.querySelectorAll("[data-print]").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.print();
+  });
+});
