7fbad968cfb80269458cd8349a4ca29e1062f3fc
Tony Mckernan <anthony.mckernan@justice.gov.uk>
2026-07-29 10:45:57 +0100

    CDPTKAN-1221 Switch on Content Security Policy (#575)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,22 +4,23 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# Rails.application.configure do
-#   config.content_security_policy do |policy|
-#     policy.default_src :self, :https
-#     policy.font_src    :self, :https, :data
-#     policy.img_src     :self, :https, :data
-#     policy.object_src  :none
-#     policy.script_src  :self, :https
-#     policy.style_src   :self, :https
-#     # Specify URI for violation reports
-#     # policy.report_uri "/csp-violation-report-endpoint"
-#   end
-#
-#   # Generate session nonces for permitted importmap and inline scripts
-#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-#   config.content_security_policy_nonce_directives = %w(script-src)
-#
-#   # Report violations without enforcing the policy.
-#   # config.content_security_policy_report_only = true
-# end
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self
+    policy.font_src    :self, :data
+    policy.img_src     :self, :data, "https://www.googletagmanager.com"
+    policy.object_src  :none
+    policy.script_src  :self, "https://www.googletagmanager.com"
+    policy.style_src   :self
+    policy.frame_src   "https://www.googletagmanager.com"
+    policy.connect_src :self,
+                       "https://www.google-analytics.com",
+                       "https://analytics.google.com",
+                       "https://www.googletagmanager.com"
+    policy.base_uri    :self
+  end
+
+  # Per-request nonce added to script-src; inline <script> tags must carry the nonce attribute.
+  config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
+  config.content_security_policy_nonce_directives = %w[script-src]
+end
